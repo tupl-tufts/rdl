71921c2bd07ad7e05b2246bde9bc131994fd3338
Jeff Foster <jfoster@cs.umd.edu>
2014-07-15 18:49:53 -0400

    Script to generate skeleton to be filled in with type annotations.

---
+++ extras/skel.rb
@@ -0,0 +1,45 @@
+def skel(klass)
+  ret = ""
+  ret << "class #{klass}"
+  ret << "< #{klass.superclass}" if klass.superclass != Object
+  ret << "\n"
+  ret << "  extend RDL\n"
+  klass.methods(false).each do |m|
+    ret << "  CLASS METHOD: typesig(:#{m})\n"
+  end
+  klass.instance_methods(false).each do |m|
+    ps = (klass.instance_method m).parameters
+    ret << "  typesig(:#{m}, \"("
+    first = true
+    block = false
+    ps.each { |kind, name|
+        if kind == :req then
+          ret << ", " unless first
+          first = false
+          ret << "#{name} : XXXX"
+        elsif kind == :opt then
+          ret << ", " unless first
+          first = false
+          ret << "#{name} : ?XXXX"
+        elsif kind == :args then
+          ret << ", " unless first
+          first = false
+          ret << "#{name} : *XXXX"
+        elsif kind == :block then
+          block = true
+        else
+          puts "ERROR! Don't know what #{kind}, #{name} means"
+        end
+    }
+    if block then
+      ret << ") { BLOCK }\")\n"
+    else
+      ret << ")\")\n"
+    end
+  end
+  ret << "end\n"
+end
+
+require "set"
+
+puts skel(Set)
