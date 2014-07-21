d012d0d3abd204641b99138912bc35aa256bb14a
Jeff Foster <jfoster@cs.umd.edu>
2014-07-21 17:20:49 -0400

    BasicObject types

---
+++ types/ruby-2.1/core/basic_object.rb
@@ -0,0 +1,14 @@
+class BasicObject
+  extend RDL
+
+  typesig(:==, "(other : %any) -> %bool")
+  typesig(:equal?, "(other : %any) -> %bool")
+  typesig(:!, "() -> %bool")
+  typesig(:!=, "(other: %any) -> %bool")
+  typesig(:instance_eval, "(String, filename : ?String, lineno : ?Fixnum) -> %any)")
+  typesig(:instance_eval, "() { () -> %any } -> %any")
+  typesig(:instance_exec, "(args: *%any) { (*%any) -> %any }" -> %any)
+  typesig(:__send__, "(Symbol, *%any) -> obj : %any")
+  rdl_alias :__id__, :object_id
+  typesig(:object_id, "() -> Fixnum")
+end
