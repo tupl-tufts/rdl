a66cfbaa0b29a768794efc2eb0a47236295a4296
Ren <spiderpig@Spiders-MacBook-Pro.local>
2014-07-23 00:43:57 -0400

    simplified

--- lib/rdl/types.rb
+++ lib/rdl/types.rb
@@ -1,5 +1,7 @@
 module RDL
   class TypesigException < StandardError; end
+  class TypeComparisonException < StandardError; end
+  class AmbiguousUnionException < StandardError; end
   class InvalidParameterException < StandardError; end
 end
 
@@ -17,28 +19,10 @@ end
  'tuple.rb',
  'type.rb',
  'type_inferencer.rb',
- 'type_parameters.rb',
- 'type_variables.rb',
  'union.rb',
  'vararg.rb'].each { |f| require_relative "types/#{f}" }
 
 class Object
-  def method_added(method_name)
-    
-  end
-
-  def rtc_meta
-    if defined? @_rtc_meta
-      @_rtc_meta
-    else
-      to_return = {}
-      to_return[:iterators] = {}
-      to_return[:_type] = nil
-
-      @_rtc_meta = to_return
-    end
-  end
-
   def rdl_type
     if self.class.name == "Symbol"
       RDL::Type::SymbolType.new(self)
@@ -47,29 +31,20 @@ class Object
 
       if class_obj.name.to_s == "Array"
         t = RDL::TypeInferencer.infer_type(self.each)
-        RDL::Type::GenericType.new(class_obj, *RDL::NativeArray[t], true)
+        RDL::Type::GenericType.new(class_obj, *[t])
       elsif class_obj.name.to_s == "Set"
         t = RDL::TypeInferencer.infer_type(self.each)
-        RDL::Type::GenericType.new(class_obj, *[t], true)
+        RDL::Type::GenericType.new(class_obj, *[t])
       elsif class_obj.name.to_s == "Hash"
         k = RDL::TypeInferencer.infer_type(self.each_key)
         v = RDL::TypeInferencer.infer_type(self.each_value)
-        RDL::Type::GenericType.new(class_obj, *RDL::NativeArray[k, v], true)
+        RDL::Type::GenericType.new(class_obj, *[k, v])
       elsif class_obj.type_parameters.size == 0 
         class_obj
       else
-        raise Exception, "User defined GenericType not supported yet"
+        raise Exception, "User defined class not supported yet"
       end
     end
-  end
-  
-  def rtc_is_complex?
-    return false if self.nil?
-    not RDL::Type::NominalType.new(self.class).type_parameters.empty?
-  end
-  
-  def is_terminal
-    false
   end
 
   def rdl_inst(types)
@@ -89,23 +64,14 @@ class Object
         end
       }
 
-      tp = self.instance_variable_get(:@s_type_parameters)
-      h = self.instance_variable_get(:@s_type_parameters).merge(h) if tp
-      self.instance_variable_set(:@s_type_parameters, h)
+      tp = self.instance_variable_get(:@__rdl_s_type_parameters)
+      h = self.instance_variable_get(:@__rdl_s_type_parameters).merge(h) if tp
+      self.instance_variable_set(:@__rdl_s_type_parameters, h)
     else
       raise Exception, "argument type to rdl_inst not support yet"
     end
 
     self
-  end
-
-  def subst_type_vars(method_type, type_var_map) 
-  end
-end
-
-class Module
-  def rdl_type
-    RDL::Type::NominalType.new(class <<self; self; end)
   end
 end
 
