cd9be550c1761c2885b4f259b996c1ebb3a12819
Ren <spiderpig@Spiders-MacBook-Pro.local>
2014-07-23 00:39:48 -0400

    simplified

--- lib/rdl/types/method_check.rb
+++ lib/rdl/types/method_check.rb
@@ -1,84 +1,6 @@
-require_relative 'native'
-
 module RDL::MethodCheck
-  def self.check_type(value, type, check_variables = true)
-    #if value.is_proxy_object?
-    #  value.rtc_type <= type
-    #elsif type.is_tuple
-
-    if type.is_tuple
-      return false unless value.is_a?(Array) and value.size == type.ordered_params.size
-
-      i = 0
-      len = value.size
-      ordered_params = type.ordered_params
-
-      while i < len
-        return false unless self.check_type(value[i], ordered_params[i], check_variables)
-        i += 1
-      end
-
-      return true
-=begin
-    elsif type.is_a?(Rtc::Types::HashType)
-      return false unless value.is_a?(Hash)
-      type.required.each {
-        |k,t|
-        return false unless value.has_key?(k)
-        return false unless self.check_type(value[k], t, check_variables)
-      }
-      type.optional.each {
-        |k,t|
-        if value.has_key?(k)
-          return false unless self.check_type(value[k], t, check_variables)
-        end
-      }
-      return true
-=end
-    #elsif $RTC_STRICT or (not value.rtc_is_complex?)
-    elsif not value.rtc_is_complex?
-      return value.rdl_type <= type
-    else
-      if type.has_variables and check_variables
-        return value.rdl_type <= type
-      end
-      case type
-      when RDL::Type::GenericType
-        #return type.nominal.klass == value.class
-        #return type.base == value.class
-        t = eval(type.base.name.to_s)
-        return t == value.class
-      when RDL::Type::UnionType
-        if type.has_variables
-          solution_found = false
-          for t in type.types
-            if self.check_type(value, t, check_variables)
-              raise Exception, "Ambiguous union detected" if solution_found
-              solution_found
-            end
-          end
-          solution_found
-        else
-          type.types.any? {|t|             
-            self.check_type(value, t, check_variables)
-          }
-        end
-      when RDL::Type::TopType
-        return true
-      when RDL::Type::TypeVariable
-        return value.rdl_type <= type
-      else
-        return false
-      end
-    end
-  end
-  
-  def self.select_and_check_args(method_types, method_name, args, has_block, class_obj)
-    method_types.each { |mt|
-      mt.type_variables.each { |tv| tv.start_solve if not tv.instantiated? and not tv.solving? }
-    }
-
-    possible_types = RDL::NativeArray.new
+  def self.select_and_check_args(method_types, method_name, args, has_block=false)
+    possible_types = []
     num_actual_args = args.length
 
     for m in method_types
@@ -86,83 +8,34 @@ module RDL::MethodCheck
       next if m.max_args != -1 and num_actual_args > m.max_args
       next if (not m.block.nil?) != has_block
 
-      #annotate_now = m.type_variables.empty?
-      #annot_vector = RDL::NativeArray.new(args.length)
+      arg_impl_valid, vartype_map = self.check_arg_impl(m, args)  
 
-      #if self.check_arg_impl(m, args, annotate_now, annot_vector)
-      if self.check_arg_impl(m, args)  
-        #possible_types.push([m, annotate_now ? annot_vector : false])
+      if arg_impl_valid
         possible_types.push(m)
       end
     end
+
     if possible_types.size > 1
       raise Exception, "cannot infer type in intersecton type for method #{method_name}, whose types are #{method_types.inspect}"
     elsif possible_types.size == 0 
       arg_types = args.map {|a| a.rdl_type}
 
-      raise RDL::TypesigException, "In method #{method_name}, annotated types are #{method_types.inspect}, but actual arguments are #{args.inspect} and has_block = #{has_block}, with types #{arg_types.inspect}" +
-        " for class #{class_obj}"
+      raise RDL::TypesigException, "In method #{method_name}, annotated types are #{method_types.inspect}, but actual arguments are #{args.inspect} and has_block = #{has_block}, with types #{arg_types.inspect}" 
     else
-      #chosen_type, annotations = possible_types[0]
       chosen_type = possible_types[0]
     end
 
-    #if not annotations
-      unsolved_variables = update_variables(chosen_type.type_variables)
-    #  annotations = annotate_args(chosen_type, args)
-    #else
-    #  unsolved_variables = []
-    #end
-
-    #return chosen_type, annotations, unsolved_variables
-
-    return chosen_type, unsolved_variables
+    vartype_map.empty? ? chosen_type : chosen_type.replace_vartypes(vartype_map)
   end
 
-  def self.check_return(m_type, return_value, unsolved_variables)
-    unsolved_variables = update_variables(unsolved_variables)
-    return false unless self.check_type(return_value, m_type.ret)
-    update_variables(unsolved_variables)
-    true
+  def self.check_return(method_type, ret_value)
+    ret_value.rdl_type.le method_type.ret
   end
 
-  def self.check_args(m_type, args, unsolved_variables)
-    if unsolved_variables.empty?
-      #annot_vector = RDL::NativeArray.new(args.length)
-      #return self.check_arg_impl(m_type, args, true, annot_vector) ? 
-      #[annot_vector,[]] : false
-      return self.check_arg_impl(m_type, args)  ? [] : false
-    else
-      #return false unless self.check_arg_impl(m_type, args, false)
-      return false unless self.check_arg_impl(m_type, args)
-      unsolved_variables = update_variables(unsolved_variables)
-      #annotations = annotate_args(m_type, args)
-      #return annotations, unsolved_variables
-      return unsolved_variables
-    end
-  end
-
-  def self.update_variables(t_vars)
-    return unless t_vars
-    unsolved_type_variables = RDL::NativeArray.new
-    t_vars.each {|tvar|
-      if tvar.solvable?
-        tvar.solve
-      elsif tvar.instantiated
-        next
-      else
-        unsolved_type_variables << tvar
-      end
-    }
-    return unsolved_type_variables
-  end
-  
-  # returns false on type error or a true value.
-  # TOOD(jtoman): comment this. it needs it *badly*
-  #def self.check_arg_impl(m_type, args,annotate_now, annot_vector = nil)
   def self.check_arg_impl(m_type, args)
     expected_arg_types = m_type.args
     arg_layout = m_type.parameter_layout
+
     i = 0
     valid = true
     num_actual_args = args.length
@@ -174,9 +47,7 @@ module RDL::MethodCheck
                         ]
                        ]
 
-    # TODO: Fix unwrap_positions
-    #unwrap_positions = m_type.unwrap
-    unwrap_positions = []
+    le_h = {}
 
     for arg_range in required_indices 
       # while loops are faster...
@@ -186,23 +57,12 @@ module RDL::MethodCheck
         type_index = type_offset + i
         value_index = start_offset + i
         if expected_arg_types[type_index].instance_of?(RDL::Type::MethodType)
-          #annot_vector[value_index] = Rtc::BlockProxy.new(args[value_index], 
-          #                                                expected_arg_types[type_index].to_actual_type, 
-          #                                                method_name, class_obj, []) if annotate_now
+          raise Exception, "not implemented"
         else
-
-          unless self.check_type(args[value_index], expected_arg_types[type_index])
+          unless args[value_index].rdl_type.le(expected_arg_types[type_index], le_h)
             valid = false
             break
           end
-
-          #if annotate_now
-          #  if unwrap_positions.include?(type_index)
-          #    annot_vector[value_index] = args[value_index]
-          #  else
-          #    annot_vector[value_index] = args[value_index]#.rtc_annotate(expected_arg_types[type_index].to_actual_type)
-          #  end
-          #end
         end
 
         i = i + 1
@@ -210,15 +70,16 @@ module RDL::MethodCheck
       break if not valid
     end
 
-    return false if not valid
+    return [false, le_h] if not valid
 
     # skip the optional shenanigans if we're done
     if arg_layout[:opt] == 0 and arg_layout[:rest] == false
-      return true
+      return [true, le_h]
     end
     
     # start index of the final required arguments
     post_req_start = num_actual_args - arg_layout[:required][1]
+
     non_req_args_count = post_req_start - arg_layout[:required][0]
     if non_req_args_count > arg_layout[:opt]
       final_opt_index = arg_layout[:required][0] + arg_layout[:opt]
@@ -226,56 +87,40 @@ module RDL::MethodCheck
       final_opt_index = post_req_start
     end
     i = arg_layout[:required][0]
+
     while i < final_opt_index
       if expected_arg_types[i].type.instance_of?(RDL::Type::MethodType)
-        #annot_vector[i] = Rtc::BlockProxy.new(args[i], expected_arg_types[i].type.to_actual_type, method_name, class_obj, []) if annotate_now
+        raise Exception, "not implemented"
       else
-        unless self.check_type(args[i], expected_arg_types[i].type)
+        unless args[i].rdl_type.le(expected_arg_types[i].type, le_h)
           valid = false
           break
         end
-        #if annotate_now
-        #  if unwrap_positions.include?(i)
-        #    annot_vector[i] = arg[i]
-        #  else
-        #    annot_vector[i] = args[i].rtc_annotate(expected_arg_types[i].type.to_actual_type)
-        #  end
-        #end
       end
       i = i + 1
     end
 
-    return false if not valid
+    return [false, le_h] if not valid
     
     if not arg_layout[:rest]
-      return true
+      return [true, le_h]
     end
+
     rest_index = arg_layout[:required][0] + arg_layout[:opt]
     rest_type = expected_arg_types[rest_index].type
     i = arg_layout[:required][0] + arg_layout[:opt]
-    #if rest_type.instance_of?(Rtc::Types::ProceduralType)
+
     if rest_type.instance_of?(RDL::Type::MethodType)
-      #if annotate_now
-      #  while i < post_req_start
-      #    annot_vector[i] = Rtc::BlockProxy.new(args[i], rest_type.to_actual_type, method_name, class_obj, [])
-      #  end
-      #end
+      raise Exception, "not implemented"
     else
       while i < post_req_start
-        unless self.check_type(args[i], rest_type)
+        unless args[i].rdl_type.le(rest_type, le_h)
           valid = false
           break
         end
-        #if annotate_now
-        #  if unwrap_positions.include?(rest_index)
-        #    annot_vector[i] = args[i]
-        #  else
-        #    annot_vector[i] = args[i].rtc_annotate(rest_type)
-        #  end
-        #end
         i = i + 1
       end
     end
-    return valid
+    return [valid, le_h]
   end
 end
