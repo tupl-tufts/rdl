f2b12c181802faa7be73d02cc0651ddf32ab6d01
Ren <spiderpig@Spiders-MacBook-Pro.local>
2014-08-01 11:56:09 -0400

    re-implemented typesig like how pre/post task was implemented

--- lib/rdl_sig.rb
+++ lib/rdl_sig.rb
@@ -1,6 +1,3 @@
-
-
-
 module RDL
 
 class Spec
@@ -30,7 +27,6 @@ class Spec
         
         @class.class_eval do
             alias_method old_mname, mname
-            
             define_method mname do |*args, &blk|
                 results = self.__send__ pre_name, *args, &blk
                 new_args = results[:args]
@@ -56,11 +52,6 @@ class Spec
                 self.__send__ post_name, res, *args, &blk
             end
         end
-    end
-    
-    def store_method_type(sig_type)
-        n = RDL::Type::NominalType.new @class
-        n.add_method_type(@mname, sig_type)
     end
     
     # DEPRECATED: Use typesig instead
@@ -114,53 +105,142 @@ class Spec
     end
     
     def typesig(sig, meta={})
-        status = @@master_switch
-        @@master_switch = false if status
-        
-        begin
-            parser = RDL::Type::Parser.new
-            t = parser.scan_str(sig)
-            tvars = meta[:vars].nil? ? [] : meta[:vars]
-            cls_params = RDL::Type::NominalType.new(@class).type_parameters
-            cls_param_symbols = cls_params.map {|p| p.symbol}
-            valid_param_symbols = tvars + cls_param_symbols
-            
-            invalid_tparams = []
-            
-            t.get_method_parameters.each {|p|
-                invalid_tparams.push(p) if not valid_param_symbols.include?(p)
-            }
-            
-            if not invalid_tparams.empty?
-                raise RDL::InvalidParameterException, "Invalid parameters #{invalid_tparams.inspect} in #{@class}##{@mname} typesig #{sig}"
-            end
-            
-            if tvars
-                tvars = tvars.map {|x| RDL::Type::TypeParameter.new(x.to_sym)}
-                t.parameters = tvars
-            end
-            
-            n = RDL::Type::NominalType.new @class
-            t_old = n.get_method(@mname)
-            store_method_type(t)  # may make an intersection type
-            t = n.get_method(@mname)
-            
-            mname = @mname
-            old_mname = "__dsl_old_#{mname}"
-            
-            ti = @class.instance_variable_get(:@typesig_info)
-            ti[mname] = [@class, mname, old_mname, cls_param_symbols, t]
-            
-            if t_old
-                @class.class_eval do
-                    alias_method mname, old_mname
-                end
-            end
-            
-            ensure
-            @@master_switch = true if status
+      mname = @mname.to_sym
+      parser = RDL::Type::Parser.new
+      type = parser.scan_str(sig)
+      tvars = meta[:vars].nil? ? [] : meta[:vars]
+
+      cls_typesigs = @class.instance_variable_get :@__typesigs
+      cls_param_symbols = @class.instance_variable_get(:@__cls_params).keys
+      valid_param_symbols = tvars + cls_param_symbols
+
+      common_tparams = tvars & cls_param_symbols
+
+      if not common_tparams.empty?
+        raise RDL::InvalidParameterException, "Parameters #{common_tparams} are used both as class type_param and method type_param"
+      end
+
+      valid_tparams = tvars + cls_param_symbols
+      invalid_tparams = []
+
+      type.get_vartypes.each {|p|
+        invalid_tparams.push(p) if not valid_param_symbols.include?(p)
+      }
+
+      if not invalid_tparams.empty?
+        raise RDL::InvalidParameterException, "Invalid parameters #{invalid_tparams.inspect} in #{@class}##{@mname} typesig #{sig}"
+      end
+      
+      if cls_typesigs.keys.include?(mname)
+        extant_type = cls_typesigs[mname]
+
+        if extant_type.instance_of? RDL::Type::IntersectionType
+          type = [type] + extant_type.types.to_a
+        else
+          type = [type, extant_type]
         end
-        RDL::MethodWrapper.wrap_method(@class, mname, old_mname, cls_param_symbols, t)
+
+        cls_typesigs[mname] = RDL::Type::IntersectionType.new(*type)
+      else
+        cls_typesigs[mname] = type
+      end
+
+      chosen_type = nil
+      method_types = cls_typesigs[mname]
+      status = nil
+      method_blk = nil
+
+      c = Proc.new {|args| 
+        if method_blk
+          chosen_type = RDL::MethodCheck.select_and_check_args(method_types, mname, args, true)
+        else
+          chosen_type = RDL::MethodCheck.select_and_check_args(method_types, mname, args)
+        end
+
+        chosen_type
+      }
+
+      bp = nil
+
+      c2 = Proc.new {|ret|
+        if bp
+          vm = bp.var_map
+          if not vm.empty?
+            chosen_type = chosen_type.replace_vartypes vm
+          end
+        end
+
+        ret_valid = RDL::MethodCheck.check_return(chosen_type, ret)
+        ret_valid
+      }
+
+      ctc = MyCtc.new(&c)
+      ctc_r = MyCtc2.new(&c2)
+      status = nil
+
+      arg_check_name = define_method_gensym("check_args") do |*args, &blk|
+        args = ctc.apply(*args)
+        
+        if blk
+          bp = BlockProxy.new(blk, chosen_type.block, self.class, mname)
+          blk = BlockProxy.wrap_block bp
+        end
+
+        { args: args, block: blk }
+      end
+
+      no_arg_check_name = define_method_gensym("check_args") do |*args, &blk|
+       { args: args, block: blk }
+      end
+
+      ret_check_name = define_method_gensym("check_ret") do |ret, *args, &blk|
+        ctc_r.apply(ret)
+      end
+
+      no_ret_check_name = define_method_gensym("check_ret") do |ret, *args, &blk|
+        ret
+      end
+
+      pre do |*args, &blk|
+        status = RDL.on?
+        
+        if status
+          RDL.turn_off
+          
+          begin
+            tp = self.instance_variable_get :@__rdl_s_type_parameters
+            tp = {} if not tp
+            uninstantiated_params = cls_param_symbols - tp.keys
+            uninstantiated_params.each {|up| tp[up] = RDL::Type::TopType.new}
+            method_types = cls_typesigs[mname]
+            method_types = method_types.replace_vartypes tp
+            method_blk = blk
+            
+            r = self.__send__ arg_check_name, *args, &blk
+          ensure
+            RDL.turn_on
+          end
+          r
+        else
+          self.__send__ no_arg_check_name, *args, &blk
+        end
+      end
+        
+      post do |ret, *args, &blk|
+        status = RDL.on?
+        if status
+          begin
+            RDL.turn_off
+            r = self.__send__ ret_check_name, ret, *args, &blk
+          ensure
+            RDL.turn_on
+          end
+          
+          r
+        else
+          self.__send__ no_ret_check_name, ret, *args, &blk
+        end
+      end
     end
     
     # Proposed changes to typesig
@@ -406,7 +486,58 @@ class Spec
         RDL::Gensym.gensym
     end
 end
+end
+
+module RDL
+  class BlockProxy < Spec
+    attr_reader :blk
+    attr_reader :blk_type
+    attr_reader :mname
+    attr_reader :class
+    attr_reader :var_map
+
+    def initialize(blk, blk_type, cls, method_name)
+      @blk = blk
+      @blk_type = blk_type
+      @class = cls
+      @mname = method_name
+      @var_map = {}
+    end
+    
+    def call(*args)
+      chosen_type = nil
+      ret_var_map = {}
+
+      c = Proc.new {|args|
+        chosen_type = RDL::MethodCheck.select_and_check_args(@blk_type, @method_name, args)
+        chosen_type
+      }
+      
+      c2 = Proc.new {|ret|
+        ret.rdl_type.le(chosen_type.ret, ret_var_map)
+      }
 
 
+      ctc = MyCtc.new(&c)
+      ctc_r = MyCtc2.new(&c2)
 
+      ctc.apply *args
+      ret = @blk.call *args
+      ctc_r.apply ret
+
+      ret_var_map.each {|k, v|
+        if @var_map.keys.include? k
+          @var_map[k] = RDL::Type::UnionType.new(v, @var_map[k])
+        else
+          @var_map[k] = v
+        end
+      }
+
+      ret
+    end
+    
+    def self.wrap_block(x)
+      Proc.new {|*v| x.call(*v)}
+    end
+  end
 end
