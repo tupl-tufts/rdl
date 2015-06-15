f2c7032b1a264d7603b5d3ab92f2c2afa70b2b2a
Jeff Foster <jfoster@cs.umd.edu>
2015-06-15 15:09:49 -0700

    Basic proc contracts.

---
+++ lib/rdl/contract/proc.rb
@@ -0,0 +1,24 @@
+module RDL::Contract
+  class ProcContract < Contract
+    attr_accessor :pre_cond, :post_cond
+
+    def initialize(pre_cond:nil, post_cond:nil)
+      @pre_cond = pre_cond
+      @post_cond = post_cond
+    end
+
+    def wrap(&blk)
+      Proc.new {|*v, &other_blk|
+        @pre_cond.check(*v, &other_blk)
+        # TODO: wrap other_blk as well?
+        tmp = blk.call(*v, &other_blk)
+        @post_cond.check(*v, tmp, &other_blk)
+        tmp
+      }
+    end
+    
+    def to_s
+      "(#{@pre_cond}) -> (#{@post_cond})"
+    end
+  end
+end
