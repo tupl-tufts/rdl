485710badcc70662929b82921277c2aa0f46a205
Jeff Foster <jfoster@cs.umd.edu>
2015-06-17 10:43:56 -0700

    Method to check whether something can be wrapped. Will be used to ensure nothing in RDL module is wrapped.

--- lib/rdl.rb
+++ lib/rdl.rb
@@ -2,3 +2,11 @@ require 'require_all'
 require_rel 'rdl/switch.rb'
 require_rel 'rdl/types/*.rb'
 require_rel 'rdl/contracts/*.rb'
+
+module RDL
+
+  def self.wrappable?(klass)
+    return klass.name =~ /^RDL::/
+  end
+
+end
