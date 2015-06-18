aab59bfd57086392d306ed7ff8907e83a1897d12
Jeff Foster <jfoster@cs.umd.edu>
2015-06-18 11:29:37 -0700

    Add methods for arrays of contracts.

--- lib/rdl/contracts/and.rb
+++ lib/rdl/contracts/and.rb
@@ -7,12 +7,21 @@ module RDL::Contract
     end
 
     def check(*v, &blk)
+      AndContract.check_array(@contracts, *v, &blk)
+    end
+
+    # Check an array of contracts a
+    def self.check_array(a, *v, &blk)
       # All contracts must be satisfied
-      @contracts.all? { |c| c.check(*v, &blk) }
+      a.all? { |c| c.check(*v, &blk) }
     end
     
     def to_s
-      @contracts.join(' && ')
+      AndContract.array_to_s(@contracts)
+    end
+
+    def self.array_to_s(a)
+      a.join(' && ')
     end
   end
 end
