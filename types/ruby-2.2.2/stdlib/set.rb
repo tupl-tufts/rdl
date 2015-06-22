dd185c18e8c851cd205a7d4179c5e17d4348d500
Jeff Foster <jfoster@cs.umd.edu>
2015-06-22 13:39:54 -0400

    Add modified set.rb file.

--- types/ruby-2.2.2/stdlib/set.rb
+++ types/ruby-2.2.2/stdlib/set.rb
@@ -1,57 +1,54 @@
-require 'rdl'
-
 class Set
-  extend RDL
   type_params [:t, :each]
 
-  typesig('self.[]', '(Array<u>) -> Set<u>, :vars => [:u])
-  typesig('self.new', '(enum : ?Enumerable<u>) -> Set<u>', :vars => [:u])
+  type 'self.[]', '(Array<u>) -> Set<u>'
+  type 'self.new', '(enum : ?Enumerable<u>) -> Set<u>'
 
   rdl_alias :&, :intersection
-  typesig(:+, '(enum : Enumerable<t>) -> Set<t>')
+  type :+, '(enum : Enumerable<t>) -> Set<t>'
   rdl_alias :-, :difference
   rdl_alias :<, :proper_subset?
   rdl_alias :<<, :add
   rdl_alias :<=, :subset?
   rdl_alias :>, :proper_superset?
   rdl_alias :>=, :superset?
-  typesig(:^, '(enum : Enumerable<t>) -> Set<t>')
-  typesig(:add, '(o : t) -> self')
-  typesig(:add?, '(o : t) -> self or nil')
-  typesig(:classify, '() { (u) -> t } -> Hash<u, Set<t>>', :vars => [:u])
-  typesig(:clear, '() -> self')
+  type :^, '(enum : Enumerable<t>) -> Set<t>'
+  type :add, '(o : t) -> self'
+  type :add?, '(o : t) -> self or nil'
+  type :classify, '() { (u) -> t } -> Hash<u, Set<t>>'
+  type :clear, '() -> self'
   rdl_alias :collect!, :map
-  typesig(:delete, '(o : t) -> self')
-  typesig(:delete?, '(o : t) -> self or nil')
-  typesig(:delete_if, '() { (t) -> %bool } -> self')
-  typesig(:difference, '(enum : Enumerable<t>) -> Set<t>')
-  typesig(:disjoint?, '(set : Set<t>) -> %bool')
-#??  typesig(:divide, '() { BLOCK }')
-  typesig(:each, '() { (t) -> %any } -> self')
-  typesig(:each, '() -> Enumerator<t>')
-  typesig(:empty?, '() -> %bool')
-#  typesig(:flatten!, '()') # How do we write a contract for this?
-#  typesig(:flatten, '()') # How do we write a contract for this?
-#  typesig(:flatten_merge, '(set : XXXX, seen : ?XXXX)') #??
+  type :delete, '(o : t) -> self'
+  type :delete?, '(o : t) -> self or nil'
+  type :delete_if, '() { (t) -> %bool } -> self'
+  type :difference, '(enum : Enumerable<t>) -> Set<t>'
+  type :disjoint?, '(set : Set<t>) -> %bool'
+#??  type :divide, '() { BLOCK }'
+  type :each, '() { (t) -> %any } -> self'
+  type :each, '() -> Enumerator<t>'
+  type :empty?, '() -> %bool'
+#  type :flatten!, '()' # How do we write a contract for this?
+#  type :flatten, '()' # How do we write a contract for this?
+#  type :flatten_merge, '(set : XXXX, seen : ?XXXX)' #??
   rdl_alias :include?, :member?
-  typesig(:intersect?, '(set : Set<t>) -> %bool')
-  typesig(:intersection, '(enum : Enumerable<t>) -> Set<t>')
-  typesig(:keep_if, '() { (t) -> %bool) } -> self')
+  type :intersect?, '(set : Set<t>) -> %bool'
+  type :intersection, '(enum : Enumerable<t>) -> Set<t>'
+  type :keep_if, '() { (t) -> %bool } -> self'
   rdl_alias :length, :size
-  typesig(:map!, '() { (t) -> u } -> Set<u>') # !! Fix, actually changes type!
-  typesig(:member?, '(o : t) -> %bool')
-  typesig(:merge, '(enum : Enumerable<t>) -> self')
-  typesig(:proper_subset?, '(set : Set<t>) -> %bool')
-  typesig(:proper_superset?, '(set : Set<t>) -> %bool')
-  typesig(:reject!, '() { (t) -> %bool } -> self or nil')
-  typesig(:replace, '(enum : Enumerable<u>) -> Set<u>') # !! Fix, actually changes type!
-  typesig(:select!, '() { (t) -> %bool) } -> self or nil')
-  typesig(:size, '() -> Fixnum')
-  typesig(:subset?, '(set : Set<t>) -> %bool')
-  typesig(:subtract, '(enum : Enumerable<t>) -> self')
-  typesig(:superset?, '(set : Set<t>) -> %bool')
-  typesig(:to_a, '() -> Array<t>')
-  typesig(:to_set, '(klass : ?Class, args : *XXXX) { BLOCK }') # ??
-  typesig(:union, '(enum : Enumerable<t>) -> Set<t>')
+  type :map!, '() { (t) -> u } -> Set<u>' # !! Fix, actually changes type!
+  type :member?, '(o : t) -> %bool'
+  type :merge, '(enum : Enumerable<t>) -> self'
+  type :proper_subset?, '(set : Set<t>) -> %bool'
+  type :proper_superset?, '(set : Set<t>) -> %bool'
+  type :reject!, '() { (t) -> %bool } -> self or nil'
+  type :replace, '(enum : Enumerable<u>) -> Set<u>' # !! Fix, actually changes type!
+  type :select!, '() { (t) -> %bool } -> self or nil'
+  type :size, '() -> Fixnum'
+  type :subset?, '(set : Set<t>) -> %bool'
+  type :subtract, '(enum : Enumerable<t>) -> self'
+  type :superset?, '(set : Set<t>) -> %bool'
+  type :to_a, '() -> Array<t>'
+#  type :to_set, '(klass : ?Class, args : *XXXX) { BLOCK }' # ??
+  type :union, '(enum : Enumerable<t>) -> Set<t>'
   rdl_alias :|, :+
 end
