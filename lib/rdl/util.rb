f8167db2d102cf1d6a748db0cae30d846dfb6690
Jeff Foster <jfoster@cs.umd.edu>
2015-06-22 09:30:52 -0400

    Move methods from RDL module to RDL::Util

---
+++ lib/rdl/util.rb
@@ -0,0 +1,16 @@
+module RDL
+  class Util
+    def self.to_class(s)
+      return s if s.class == Class
+      return s.to_s.split("::").inject(Object) { |base, name| base.const_get(name) }
+    end
+
+    def self.method_defined?(klass, method)
+      begin
+        (self.to_class klass).method_defined? method.to_sym
+      rescue NameError
+        return false
+      end
+    end
+  end
+end
