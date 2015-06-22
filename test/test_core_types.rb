89bd5e5bb436595295cd967ab083022d6b7636bf
Jeff Foster <jfoster@cs.umd.edu>
2015-06-22 15:01:44 -0400

    Tests for core and stdlib, but commented out

---
+++ test/test_core_types.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require_relative '../lib/rdl.rb'
+#require_relative '../types/ruby-2.2.2/core/string.rb'
+
+class TestCoreTypes < Minitest::Test
+end
---
+++ test/test_stdlib_types.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require_relative '../lib/rdl.rb'
+#require_relative '../types/ruby-2.2.2/stdlib/set.rb'
+
+class TestStdlibTypes < Minitest::Test
+end
