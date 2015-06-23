4da5a7e9da64a109a994deed03e37523b57615c4
Jeff Foster <jfoster@cs.umd.edu>
2015-06-23 07:20:30 -0400

    Start testing parsing of core library types.

--- test/test_core_types.rb
+++ test/test_core_types.rb
@@ -1,5 +1,10 @@
 require 'minitest/autorun'
 require_relative '../lib/rdl.rb'
+#require_relative '../types/ruby-2.2.2/core/array.rb'
+#require_relative '../types/ruby-2.2.2/core/basic_object.rb'
+#require_relative '../types/ruby-2.2.2/core/hash.rb'
+#require_relative '../types/ruby-2.2.2/core/kernel.rb'
+#require_relative '../types/ruby-2.2.2/core/object.rb'
 #require_relative '../types/ruby-2.2.2/core/string.rb'
 
 class TestCoreTypes < Minitest::Test
