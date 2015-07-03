5a8bc884489ee02bbb6936bd16454a424b8233f6
Jeff Foster <jfoster@cs.umd.edu>
2015-07-02 22:10:41 -0400

    Some more core typesigs

--- test/test_core_types.rb
+++ test/test_core_types.rb
@@ -1,11 +1,13 @@
 require 'minitest/autorun'
 require_relative '../lib/rdl.rb'
-#require_relative '../types/ruby-2.2.2/core/array.rb'
-#require_relative '../types/ruby-2.2.2/core/basic_object.rb'
-#require_relative '../types/ruby-2.2.2/core/hash.rb'
-#require_relative '../types/ruby-2.2.2/core/kernel.rb'
-#require_relative '../types/ruby-2.2.2/core/object.rb'
-#require_relative '../types/ruby-2.2.2/core/string.rb'
+# require_relative '../types/ruby-2.2.2/core/array.rb'
+# require_relative '../types/ruby-2.2.2/core/basic_object.rb'
+# require_relative '../types/ruby-2.2.2/core/hash.rb'
+# require_relative '../types/ruby-2.2.2/core/kernel.rb'
+# require_relative '../types/ruby-2.2.2/core/object.rb'
+# require_relative '../types/ruby-2.2.2/core/string.rb'
+# require_relative '../types/ruby-2.2.2/core/enumerable.rb'
+# require_relative '../types/ruby-2.2.2/core/enumerator.rb'
 
 class TestCoreTypes < Minitest::Test
   # TODO: Add a bunch of example code that uses strings
