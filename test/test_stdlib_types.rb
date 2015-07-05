5fc7ec2c78d0ce3bccf959750244c67c6b6540a8
Jeff Foster <jfoster@cs.umd.edu>
2015-07-05 18:31:47 -0400

    More code to invoke set methods.

--- test/test_stdlib_types.rb
+++ test/test_stdlib_types.rb
@@ -1,7 +1,6 @@
 require 'minitest/autorun'
 require_relative '../lib/rdl.rb'
-require_relative '../types/ruby-2.2.2/core/enumerable.rb'
-require_relative '../types/ruby-2.2.2/core/enumerator.rb'
+require_relative '../types/ruby-2.2.2/core.rb'
 require_relative '../types/ruby-2.2.2/stdlib/set.rb'
 
 class TestStdlibTypes < Minitest::Test
@@ -21,5 +20,26 @@ class TestStdlibTypes < Minitest::Test
     set = numbers.divide { |i,j| (i - j).abs == 1 }
     Set[1, 2, 3].intersect? Set[4, 5] # => false
     Set[1, 2, 3].intersect? Set[3, 4] # => true
+    # Some more tests, just to make sure type checking doesn't cause crashes
+    s3 = s1 - s2
+    s1.proper_subset? s2
+    s1.superset? s2
+    s1 ^ s2
+    s1.add?("bar")
+    h = s1.classify { |x| x.size }
+    s2.clear
+    s4 = s1.map { |x| 42 }
+    s1.delete "foo"
+    s1.delete? "bar"
+    s1.delete_if { |x| false }
+    s1.each { |x| nil }
+    s1.empty?
+    s1.member? 42
+    s1.intersection [1,2,3]
+    s1.keep_if { |x| true }
+    s1.size
+    s1.difference [1,2,3]
+    s1.to_a
+    s5 = s1 + s2
   end
 end
