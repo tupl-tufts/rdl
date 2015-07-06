80a79ab012bacf517d21eef9506f62224881e699
Jeff Foster <jfoster@cs.umd.edu>
2015-07-06 14:50:36 -0400

    Nowrap default set

--- lib/rdl/config.rb
+++ lib/rdl/config.rb
@@ -7,8 +7,12 @@ class RDL::Config
 
   def initialize
 #    @nowrap = Set.new
-    @nowrap = Set.new [Array, BasicObject, Dir, Enumerable, Enumerator, File,
-                       Hash, Kernel, MatchData, Object, Range, Regexp, String, Symbol]
+
+    @nowrap = Set.new [Array, BasicObject, Dir, Encoding, Enumerable,
+                       Enumerator, File, File::Stat, Hash, Kernel,
+                       MatchData, Object, Range, Regexp, String,
+                       Symbol, Time]
+
   end
 
   def add_nowrap(*klasses)
