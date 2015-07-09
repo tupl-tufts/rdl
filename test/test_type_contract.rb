febd2dc4a471cddcd6947950f796de41e5fb1d97
Jeff Foster <jfoster@cs.umd.edu>
2015-07-09 19:13:36 -0400

    Tests for named parameters; not all tests pass yet.

--- test/test_type_contract.rb
+++ test/test_type_contract.rb
@@ -100,7 +100,82 @@ class TestTypeContract < Minitest::Test
     assert_equal 42, p9.call(42, 43)
     assert_raises(TypeError) { p9.call(42, 43, 44) }
     assert_raises(TypeError) { p9.call }
+  end
 
-    #TODO: Names
+  def test_proc_names
+    t1 = @p.scan_str "(x: Fixnum) -> Fixnum"
+    p1 = t1.to_contract.wrap { |x:| x }
+    assert_equal 42, p1.call(x: 42)
+    assert_raises(TypeError) { p1.call(x: "42") }
+    assert_raises(TypeError) { p1.call() }
+    assert_raises(TypeError) { p1.call(x: 42, y: 42) }
+    assert_raises(TypeError) { p1.call(y: 42) }
+    assert_raises(TypeError) { p1.call(42) }
+    assert_raises(TypeError) { p1.call(42, x: 42) }
+    t2 = @p.scan_str "(x: Fixnum, y: String) -> Fixnum"
+    p2 = t2.to_contract.wrap { |x:,y:| x }
+    assert_equal 42, p2.call(x: 42, y: "33")
+    assert_raises(TypeError) { p2.call() }
+    assert_raises(TypeError) { p2.call(x: 42) }
+    assert_raises(TypeError) { p2.call(x: "42", y: "33") }
+    assert_raises(TypeError) { p2.call(42, "43") }
+    assert_raises(TypeError) { p2.call(42, x: 42, y: "33") }
+    assert_raises(TypeError) { p2.call(x: 42, y: "33", z: 44) }
+    t3 = @p.scan_str "(Fixnum, y: String) -> Fixnum"
+    p3 = t3.to_contract.wrap { |x, y:| x }
+    assert_equal 42, p3.call(42, y:"43")
+    assert_raises(TypeError) { p3.call(42) }
+    assert_raises(TypeError) { p3.call(42, y: 43) }
+    assert_raises(TypeError) { p3.call() }
+    assert_raises(TypeError) { p3.call(42, 43, y: 44) }
+    assert_raises(TypeError) { p3.call(42, y: 43, z: 44) }
+    t4 = @p.scan_str "(Fixnum, x: Fixnum, y: String) -> Fixnum"
+    p4 = t4.to_contract.wrap { |a, x:, y:| a }
+    assert_equal 42, p4.call(42, x: 43, y: "44")
+    assert_raises(TypeError) { p4.call(42, x: 43) }
+    assert_raises(TypeError) { p4.call(42, y: "43") }
+    assert_raises(TypeError) { p4.call() }
+    assert_raises(TypeError) { p4.call(42, 43, x: 44, y: "45") }
+    assert_raises(TypeError) { p4.call(42, x: 43, y: "44", z: 45) }
+    t5 = @p.scan_str "(x: Fixnum, y: ?String) -> Fixnum"
+    p5 = t5.to_contract.wrap { |x:, **ys| x }
+    assert_equal 42, p5.call(x: 42, y: "43")
+    assert_equal 42, p5.call(x: 42)
+    assert_raises(TypeError) { p5.call() }
+    assert_raises(TypeError) { p5.call(x: 42, y: 43) }
+    assert_raises(TypeError) { p5.call(x: 42, y: 43, z: 44) }
+    assert_raises(TypeError) { p5.call(3, x: 42, y: 43) }
+    assert_raises(TypeError) { p5.call(3, x: 42) }
+    t6 = @p.scan_str "(x: ?Fixnum, y: String) -> Fixnum"
+    p6 = t6.to_contract.wrap { |y:, **xs| 42 }
+    assert_equal 42, p6.call(x: 43, y: "44")
+    assert_equal 42, p6.call(y: "44")
+    assert_raises(TypeError) { p6.call() }
+    assert_raises(TypeError) { p6.call(x: "43", y: "44") }
+    assert_raises(TypeError) { p6.call(42, x: 43, y: "44") }
+    assert_raises(TypeError) { p6.call(x: 43, y: "44", z: 45) }
+    skip "Remaining tests fail"
+    t7 = @p.scan_str "(x: ?Fixnum, y: ?String) -> Fixnum"
+    p7 = t7.to_contract.wrap { |**args| 42 }
+    assert_equal 42, p7.call()
+    assert_equal 42, p7.call(x: 43)
+    assert_equal 42, p7.call(y: "44")
+    assert_equal 42, p7.call(x: 43, y: "44")
+    assert_raises(TypeError) { p7.call(x: "43", y: "44") }
+    assert_raises(TypeError) { p7.call(41, x: 43, y: "44") }
+    assert_raises(TypeError) { p7.call(x: 43, y: "44", z: 45) }
+    t8 = @p.scan_str "(?Fixnum, x: ?Symbol, y: ?String) -> Fixnum"
+    p8 = t8.to_contract.wrap { |*args| 42 }
+    assert_equal 42, p8.call()
+    assert_equal 42, p8.call(43)
+    assert_equal 42, p8.call(x: :foo)
+    assert_equal 42, p8.call(43, x: :foo)
+    assert_equal 42, p8.call(y: "foo")
+    assert_equal 42, p8.call(43, y: "foo")
+    assert_equal 42, p8.call(x: :foo, y: "foo")
+    assert_equal 42, p8.call(43, x: :foo, y: "foo")
+    assert_raises(TypeError) { p8.call(43, 44, x: :foo, y: "foo") }
+    assert_raises(TypeError) { p8.call(43, x: "foo", y: "foo") }
+    assert_raises(TypeError) { p8.call(43, x: :foo, y: "foo", z: 44) }
   end
 end
