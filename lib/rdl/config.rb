e197a92b5a52aa6d2cd5ad794907361b45a432e2
Alex <Leaf@Fox-Lab.local>
2015-09-30 22:21:14 -0400

    Added statistics functionality

--- lib/rdl/config.rb
+++ lib/rdl/config.rb
@@ -18,4 +18,35 @@ class RDL::Config
   def remove_nowrap(*klasses)
     klasses.each { |klass| @nowrap.delete klass }
   end
+  
+  def profile_stats(outname="/#{__FILE__[0...-3]}",outdir="")
+    require 'profile'
+    Profiler__.stop_profile # Leave setup out of stats
+    
+    at_exit do
+      Profiler__.stop_profile
+      $__rdl_contract_switch.off {
+        totals = {}
+        Profiler__.class_variable_get(:@@maps).values.each do |threadmap|
+          threadmap.each do |key, data|
+            total_data = (totals[key.to_s] ||= [-1, 0, 0.0, 0.0, key])
+            total_data[1] += data[0]
+            total_data[2] += data[1]
+            total_data[3] += data[2]
+          end
+        end
+        $__rdl_wrapped_calls.each{ |mname,ct|
+          if (totals[mname]) then totals[mname][0] = ct end
+        }
+        
+        require 'json'
+        fpath = "#{outdir}/#{outname}_rdlstat.json".gsub("//","")
+        File.open(fpath,'w') do |file|
+          file.puts totals.to_json
+        end
+      }
+    end
+    
+    Profiler__.start_profile # Restart profiler after setup
+  end
 end
