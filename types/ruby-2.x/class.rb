e3cce7c491071b3201b20258b479afeeaed64876
Jeff Foster <jfoster@cs.umd.edu>
2015-12-17 14:00:40 -0500

    Be more consistent in use of symbols, strings, single quotes, etc

--- types/ruby-2.x/class.rb
+++ types/ruby-2.x/class.rb
@@ -1,11 +1,11 @@
 class Class
   rdl_nowrap
 
-  type 'allocate', '() -> %any' # Instance of class self
-  type 'inherited', '(Class) -> %any'
+  type :allocate, '() -> %any' # Instance of class self
+  type :inherited, '(Class) -> %any'
   #type 'initialize', '() -> '
   #type 'new', '(*%any) -> %any' #Causes two other test cases to fail
-  type 'superclass', '() -> Class or nil'
+  type :superclass, '() -> Class or nil'
 
   #  type :class_eval, '() {() -> %any} -> %any'
   #  type :method_defined?, '(String or Symbol) -> %bool'
--- types/ruby-2.x/exception.rb
+++ types/ruby-2.x/exception.rb
@@ -1,17 +1,17 @@
 class Exception
   rdl_nowrap
-  
-  type '==', '(%any) -> %bool'
-  type 'backtrace', '() -> Array<String>'
-  type 'backtrace_locations', '() -> Array<Thread::Backtrace::Location>'
-  type 'cause', '() -> nil' # TODO exception is proper postcondition
-  type 'exception', '(?String) -> Exception' # or error
+
+  type :==, '(%any) -> %bool'
+  type :backtrace, '() -> Array<String>'
+  type :backtrace_locations, '() -> Array<Thread::Backtrace::Location>'
+  type :cause, '() -> nil' # TODO exception is proper postcondition
+  type :exception, '(?String) -> Exception' # or error
   # type 'initialize', '() -> '
-  type 'inspect', '() -> String'
-  type 'message', '() -> String'
+  type :inspect, '() -> String'
+  type :message, '() -> String'
   # type 'method_missing', '() -> '
   # type 'respond_to?', '() -> '
   # type 'respond_to_missing?', '() -> '
-  type 'set_backtrace', '(String or Array<String>) -> Array<String>'
-  type 'to_s', '() -> String'
+  type :set_backtrace, '(String or Array<String>) -> Array<String>'
+  type :to_s, '() -> String'
 end
