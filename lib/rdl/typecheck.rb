8d8b4a267c6756da8fe105ecd94addb803df6040
Jeff Foster <jfoster@cs.umd.edu>
2016-05-24 15:34:45 -0400

    Actually do some type checking! Also, cool error reporting using parser's diagnostic message printing.

--- lib/rdl/typecheck.rb
+++ lib/rdl/typecheck.rb
@@ -2,6 +2,10 @@ class RDL::Typecheck
 
   class StaticTypeError < StandardError; end
 
+  @@type_true = RDL::Type::NominalType.new TrueClass
+  @@type_false = RDL::Type::NominalType.new FalseClass
+  @@type_string = RDL::Type::NominalType.new String
+
   class ASTMapper < AST::Processor
     attr_accessor :line_defs
 
@@ -25,6 +29,11 @@ class RDL::Typecheck
     end
   end
 
+  # report msg at ast's loc
+  def self.error(reason, args, ast)
+    raise StaticTypeError, ("\n" + (Parser::Diagnostic.new :error, reason, args, ast.loc.expression).render.join("\n"))
+  end
+
   def self.typecheck(klass, meth)
     file, line = $__rdl_meths.get(klass, meth, :source_location)
     digest = Digest::MD5.file file
@@ -40,11 +49,43 @@ class RDL::Typecheck
     ast = $__rdl_ruby_parser_cache[file][1][:line_defs][line]
     types = $__rdl_meths.get(klass, meth, :type)
     raise RuntimeError, "Can't typecheck method with no types?!" if types.nil? or types == []
-#    puts
-#    puts ast
-#    types.each { |type|
-#      puts type
-#    }
+
+    name, args, body = *ast
+    raise RuntimeError, "Method #{name} defined where method #{meth} expected" if name.to_sym != meth
+    types.each { |type|
+      # TODO will need fancier logic here for matching up more complex arg lists
+      a = args.children.map { |arg| arg.children[0] }.zip(type.args).to_h
+      _, body_type = tc(a, body)
+      error :bad_return_type, [body_type.to_s, type.ret.to_s], body unless body_type <= type.ret
+    }
+  end
+
+  # The actual type checking logic.
+  # [+ a +] is the (local variable) type environment mapping variables (symbols) to types
+  # [+ e +] is the expression to type check
+  # Returns [a', t], where a' is the type environment at the end of the expression
+  # and t is the type of the expression
+  def self.tc(a, e)
+    case e.type
+    when :true
+      [a, @@type_true]
+    when :false
+      [a, @@type_false]
+    when :str, :string
+      [a, @@type_string]
+    when :lvar  # local variable
+      [a, a[e.children[0]]]
+    else
+      raise RuntimeError, "Expression kind #{e.type} unsupported"
+    end
   end
 
 end
+
+# Modify Parser::MESSAGES so can use the awesome parser diagnostics printing!
+type_error_messages = {
+  bad_return_type: 'Got type %s where return type %s expected',
+}
+old_messages = Parser::MESSAGES
+Parser.send(:remove_const, :MESSAGES)
+Parser.const_set :MESSAGES, (old_messages.merge(type_error_messages))
--- test/test_typecheck.rb
+++ test/test_typecheck.rb
@@ -10,11 +10,24 @@ class TestTypecheck < Minitest::Test
     }
     assert_equal 42, id_ff(42)
 
+    assert_raises(RDL::Typecheck::StaticTypeError) {
+      self.class.class_eval {
+        type "(Fixnum) -> Fixnum", typecheck_now: true
+        def id_fs(x) "42"; end
+      }
+    }
+
     skip "not implemented yet"
     self.class.class_eval {
       type "(Fixnum) -> Fixnum", typecheck_now: true
       def id_fs(x) "42"; end
     }
     assert_raises(RDL::Typecheck::StaticTypeError) { id_fs(42) }
+
+    self.class.class_eval {
+      type "(Fixnum, Fixnum) -> Fixnum", typecheck_now: true
+      def add(x, y) x+y; end
+    }
+    assert_equal 42, id_ff(42)
   end
 end
