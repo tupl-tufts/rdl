4d2855de3986cd274eb03d557f23bf999306b8c3
Jeff Foster <jfoster@cs.umd.edu>
2016-08-09 14:39:27 +0200

    delete dead comment.

--- types/rails-5.x/active_record/model_schema.rb
+++ types/rails-5.x/active_record/model_schema.rb
@@ -26,27 +26,3 @@ module ActiveRecord
     end
   end
 end
-
-# module ActiveRecord
-#   class Base
-# # class ApplicationRecord < ActiveRecord::Base
-#
-#     # Create types for the object's database columns
-#     # We could do this in ActiveRecord::Schema.define, but that's a little more complex
-#
-#     singleton_class.send :alias_method, :__rdl_inherited_saved, :inherited # save current inherited method
-#
-#     def self.inherited(subclass)
-#       puts "inherited by #{subclass}, abstract_class: #{subclass.abstract_class?.inspect}"
-#       unless subclass.abstract_class?
-#         puts "Columns for #{subclass}"
-#         subclass.columns.each { |k, t|
-#           puts "#{k.inspect} => #{t.inspect}"
-#         }
-#         puts "Done columns for #{subclass}"
-#       end
-#       return __rdl_inherited_saved(subclass)
-#     end
-#
-#   end
-# end
