461c2177c1af19e7e16c770f9882a9440525aaf8
Jeff Foster <jfoster@cs.umd.edu>
2016-08-09 11:54:35 +0200

    load recursively from subdirectories in rdl_types

--- lib/rdl_types.rb
+++ lib/rdl_types.rb
@@ -1,4 +1,4 @@
 dir = RUBY_VERSION.split('.')[0] + ".x"
 
 require_relative "../types/ruby-#{dir}/_aliases.rb" # load type aliases first
-Dir[File.dirname(__FILE__) + "/../types/ruby-#{dir}/*.rb"].each { |f| require f }
+Dir[File.dirname(__FILE__) + "/../types/ruby-#{dir}/**/*.rb"].each { |f| require f }
