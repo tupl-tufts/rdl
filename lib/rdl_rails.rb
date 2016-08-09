aa4c6edfa934c9133fa47fc9a8f34d394eb374bc
Jeff Foster <jfoster@cs.umd.edu>
2016-08-09 11:54:09 +0200

    rdl/rails_types -> rdl_types

--- lib/rdl_rails.rb
+++ lib/rdl_rails.rb
@@ -1,1 +1,12 @@
-require_rel "../types/rails-#{Rails::VERSION::STRING}/*.rb"
+if Rails.env.development? || Rails.env.test?
+  require 'rdl'
+  require 'rdl_types'
+
+  dir = Rails::VERSION::STRING.split('.')[0] + ".x"
+  require_relative "../types/rails-#{dir}/_helpers.rb" # load type aliases first
+  Dir[File.dirname(__FILE__) + "/../types/rails-#{dir}/**/*.rb"].each { |f| require f }
+elsif Rails.env.production?
+  require 'rdl_disable'
+else
+  raise RuntimeError, "Don't know what to do in Rails environment #{Rails.env}"
+end
