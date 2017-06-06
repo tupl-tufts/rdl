855c45a4baaa8dcfd38e28b6acbf7ec16d103d3e
Spider Pig <spiderpig@wireless-10-105-203-173.umd.edu>
2017-06-06 03:09:02 -0400

    new typesigs

--- lib/types/core/strscan.rb
+++ lib/types/core/strscan.rb
@@ -1,5 +1,6 @@
 rdl_nowrap :StringScanner
 
+type :StringScanner, 'self.new', '(String, ?%bool) -> StringScanner'
 type :StringScanner, :eos?, '() -> %bool'
 type :StringScanner, :scan, '(Regexp) -> String'
 type :StringScanner, :getch, '() -> String'
