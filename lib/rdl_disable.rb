766cb4f944161e9aa0dc8a3a243068020902dc55
Jeff Foster <jfoster@cs.umd.edu>
2017-06-08 20:38:35 -0400

    Make rdl_disable structure match rdl.

--- lib/rdl_disable.rb
+++ lib/rdl_disable.rb
@@ -1,25 +1,18 @@
 # Defines RDL methods that do nothing
 
-module RDL; end
+module RDL
+  def nowrap(*args); end
+  def at(*args); end
+  def do_typecheck(*args); end
+  def note_type(*args); end
+  def remove_type(*args); end
+end
 
-def RDL.config(*args); end
-
-class Object
+module RDL::Annotate
   def pre(*args); end
   def post(*args); end
   def type(*args); end
   def var_type(*args); end
-  def rdl_alias(*args); end
-  def type_params(*args); end
-  def rdl_nowrap(*args); end
-  def instantaite!(*args); self; end
-  def deinstantaite!(*args); self; end
-  def type_cast(*args); self; end
-  def type_alias(*args); end
-  def rdl_do_typecheck(*args); end
-  def rdl_note_type(*args); end
-  def rdl_remove_type(*args); end
-
   def attr_accessor_type(*args)
     args.each_slice(2) { |name, typ| attr_accessor name }
     nil
@@ -36,4 +29,21 @@ class Object
     args.each_slice(2) { |name, typ| attr_writer name }
     nil
   end
+
+  def rdl_alias(*args); end
+  def type_params(*args); end
+  def type_alias(*args); end
+
+end
+
+module RDL
+  extend RDL::Annotate
+end
+
+def RDL.config(*args); end
+
+class Object
+  def rdl_instantaite!(*args); self; end
+  def rdl_deinstantaite!(*args); self; end
+  def rdl_type_cast(*args); self; end
 end
