bd9875b7502ce8dc04458c16ece7e16f6fd5a376
Jeff Foster <jfoster@cs.umd.edu>
2017-07-03 20:40:45 -0400

    Remove some stray Fixnums in rails types

--- lib/types/rails/active_record/relation.rb
+++ lib/types/rails/active_record/relation.rb
@@ -2,10 +2,10 @@ RDL.nowrap :'ActiveRecord::Relation'
 
 RDL.type_params :'ActiveRecord::Relation', [:t], :all?
 
-RDL.type :'ActiveRecord::Relation', :[], '(Fixnum) -> t'
+RDL.type :'ActiveRecord::Relation', :[], '(Integer) -> t'
 RDL.type :'ActiveRecord::Relation', :empty?, '() -> %bool'
 RDL.type :'ActiveRecord::Relation', :first, '() -> t'
-RDL.type :'ActiveRecord::Relation', :length, '() -> Fixnum'
-RDL.type :'ActiveRecord::Relation', :sort, '() {(t, t) -> Fixnum} -> Array<t>'
+RDL.type :'ActiveRecord::Relation', :length, '() -> Integer'
+RDL.type :'ActiveRecord::Relation', :sort, '() {(t, t) -> Integer} -> Array<t>'
 RDL.type :'ActiveRecord::Relation', :each, '() -> Enumerator<t>'
 RDL.type :'ActiveRecord::Relation', :each, '() { (t) -> %any } -> Array<t>'
