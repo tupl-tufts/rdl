41523f604cc896990c5e068f6d1cc0224f473420
Geoff Huston <geoff.huston@gmail.com>
2020-04-15 11:23:40 -0400

    cleaned up some lint

--- test/test_infer.rb
+++ test/test_infer.rb
@@ -9,18 +9,13 @@ $LOAD_PATH << File.dirname(__FILE__) + '/../lib'
 require 'rdl'
 require 'types/core'
 
-# module Kernel
-#   def _descc
-#     ObjectSpace.each_object(Class).select { |klass| klass < self }
-#   end
-# end
-
+# Testing Inference (constraint.rb)
 class TestInfer < Minitest::Test
   extend RDL::Annotate
 
   def setup
     RDL.reset
-    RDL::Config.instance.number_mode = true  # treat all numeric classes the same
+    RDL::Config.instance.number_mode = true # treat all numeric classes the same
 
     ### Uncomment below to see test names. Useful for hanging tests.
     # puts "Start #{@NAME}"
@@ -35,27 +30,25 @@ class TestInfer < Minitest::Test
     RDL.infer self.class, method, time: :test
     RDL.do_infer :test, render_report: false
 
-    types = RDL::Globals.info.get "TestInfer", method, :type
+    types = RDL::Globals.info.get 'TestInfer', method, :type
     assert types.length == 1, msg: 'Expected one solution for type'
 
     types[0]
   end
 
-  def assert_type_equal(meth, b)
+  def assert_type_equal(meth, expected_type)
     typ = infer_method_type meth
-    a = typ.solution
 
-    ast = RDL::Typecheck.get_ast(self.class, meth)
+    ast  = RDL::Typecheck.get_ast(self.class, meth)
     code = CodeRay.scan(ast.loc.expression.source, :ruby).term
 
-    error_str = 'Given'.yellow + ":\n  #{code}\n\n"
+    error_str  = 'Given'.yellow + ":\n  #{code}\n\n"
+    error_str += 'Expected '.green + expected_type.to_s + "\n"
+    error_str += 'Got      '.red + typ.solution.to_s
 
-    error_str += 'Expected '.green + b.to_s + "\n"
-    error_str += 'Got      '.red + a.to_s
+    binding.pry unless expected_type == typ.solution
 
-    binding.pry unless a === b
-
-    assert a == b, error_str
+    assert expected_type == typ.solution, error_str
   end
 
   def self.should_have_type(meth, typ)
@@ -64,7 +57,7 @@ class TestInfer < Minitest::Test
     end
   end
 
-# ----------------------------------------------------------------------------
+  # ----------------------------------------------------------------------------
 
   def return_two
     2
