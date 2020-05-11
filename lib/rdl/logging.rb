7e585ae0b173fdba5a79af75ac206d3d4ef5d20a
Geoff Huston <geoff.huston@gmail.com>
2020-05-11 11:14:37 -0400

    log can optionally write to a file at its own log level

--- lib/rdl/logging.rb
+++ lib/rdl/logging.rb
@@ -19,6 +19,12 @@ class RDL::Logging
     colors[a]
   end
 
+  def self.log_to_file(file, level: nil)
+    File.new file, "w"
+    @log_file = file
+    @log_file_level = level
+  end
+
   def self.log_level_leq(a, b)
     a_idx = LEVELS.find_index(a)
     b_idx = LEVELS.find_index(b)
@@ -31,24 +37,23 @@ class RDL::Logging
     match && [match[1] || '', match[3] || ''] || ['', '']
   end
 
-  def self.log_str(area, level, message, message_color: nil)
-    tracing  = RDL::Config.instance.log_levels[area] == :trace
-    no_color = RDL::Config.instance.disable_log_colors
-
-    place      = caller[1]
+  def self.log_str(no_color, tracing, message_level, message, message_color: nil)
+    place      = caller.find { |s| !s.match(/logging.rb/) }
     file, line = extract_file_line(place)
     meth       = place.match(/in `(block.*?in )?(.*?)'/)
 
     meth = meth ? meth[2] : 'unknown'
 
-    lc = log_level_colors(level)
+    lc = log_level_colors(message_level)
 
     meth      = meth.to_s.colorize(lc) unless no_color
-    file_line = "#{file}:#{line}".colorize(:light_black) unless no_color
+    file_line = "#{file}:#{line} "
+    file_line = file_line.colorize(:light_black) unless no_color
     message   = message.colorize(message_color) if message_color && !no_color
+    message   = message.uncolorize if no_color
 
     level_str = ''
-    level_str = "#{level.to_s.upcase} " if no_color
+    level_str = "#{message_level.to_s.upcase} " if no_color
 
     depth_string = ''
     depth_string = " #{caller.length - 1}" if tracing
@@ -60,32 +65,79 @@ class RDL::Logging
     [spacers + leader + ' ' + message, (spacers + leader).uncolorize.length + 1]
   end
 
-  def self.log_header(area, level, header)
-    return unless log_level_leq(RDL::Config.instance.log_levels[area] || :info, level)
-    no_color = RDL::Config.instance.disable_log_colors
-    if no_color
-      stars = "***************"
-      str, = log_str(area, level, header, message_color: { mode: :bold }) + ' ' + stars
-      puts "\n" + stars + ' ' + str
+  def self.generate_no_color_header(tracing, message_level, header)
+    no_color_str, = log_str(true, tracing, message_level, header, message_color: { mode: :bold })
+    "\n*************** " + no_color_str
+  end
+
+  def self.log_header(area, message_level, header)
+    log_header_to_file(area, message_level, header)
+
+    log_level = RDL::Config.instance.log_levels[area] || :info
+    tracing = log_level == :trace
+
+    return unless log_level_leq(log_level, message_level)
+
+    if RDL::Config.instance.disable_log_colors
+      puts generate_no_color_header(tracing, message_level)
     else
-      str, = log_str(area, level, header, message_color: { mode: :bold })
+      str, = log_str(false, tracing, message_level, header, message_color: { mode: :bold })
       puts "\n" + str
+    end
+
+    if @log_file
     end
   end
 
-  def self.log(area, level, message, ast: nil)
-    return unless log_level_leq(RDL::Config.instance.log_levels[area] || :info, level)
+  def self.log_header_to_file(area, message_level, header)
+    return unless @log_file
 
-    str, len = log_str(area, level, message, message_color: :white)
+    log_level = @log_file_level || RDL::Config.instance.log_levels[area] || :info
+
+    return unless log_level_leq(log_level, message_level)
+
+    tracing = log_level == :trace
+
+    no_color_str = generate_no_color_header(tracing, message_level, header)
+    File.open(@log_file, "a+") { |f| f.puts no_color_str }
+  end
+
+  def self.log(area, message_level, message, ast: nil)
+    log_message_to_file(area, message_level, message, ast)
+    log_level = RDL::Config.instance.log_levels[area] || :info
+
+    return unless log_level_leq(log_level, message_level)
+
+    tracing = log_level == :trace
+
+    no_color = RDL::Config.instance.disable_log_colors
+    str, len = log_str(no_color, tracing, message_level, message, message_color: :white)
 
     puts str
-    puts ast_render len, ast.loc.expression if ast
-    exit 1 if ast
+    puts ast_render len, ast.loc.expression, no_color if ast
+  end
+
+  def self.log_message_to_file(area, message_level, message, ast)
+    return unless @log_file
+
+    log_level = @log_file_level || RDL::Config.instance.log_levels[area] || :info
+
+    return unless log_level_leq(log_level, message_level)
+
+    tracing = log_level == :trace
+
+    str, = log_str(true, tracing, message_level, message)
+    ast_str = ast ? ast_render(0, ast.loc.expression) : nil
+
+    File.open(@log_file, "a+") do |f|
+      f.puts str
+      f.puts ast_str if ast_str
+    end
   end
 
   ###### Borrowed from Diagnostic.rb ###########################################
 
-  def self.ast_render(offset, location)
+  def self.ast_render(offset, location, no_color)
     first_line = first_line_only(location)
     last_line  = last_line_only(location)
     num_lines  = (location.last_line - location.line) + 1
@@ -98,13 +150,13 @@ class RDL::Logging
 
     file_name, = extract_file_line(buffer.name)
     source_loc = "#{file_name}:"
-    unless RDL::Config.instance.disable_log_colors
+    unless no_color
       source_loc = source_loc.colorize(:light_black)
     end
 
     [' ' * offset + source_loc + "#{lineno}:#{column} - #{last_lineno}:#{last_column}"] +
-      render_line(offset, first_line, num_lines > 2, false) +
-      render_line(offset, last_line, false, true)
+      render_line(offset, first_line, no_color, num_lines > 2, false) +
+      render_line(offset, last_line, no_color, false, true)
   end
 
   ##
@@ -112,7 +164,7 @@ class RDL::Logging
   #
   # @return [Array<String>]
   #
-  def self.render_line(offset, range, ellipsis=false, range_end=false)
+  def self.render_line(offset, range, no_color, ellipsis=false, range_end=false)
     source_line    = range.source_line
     highlight_line = ' ' * source_line.length
 
@@ -138,7 +190,7 @@ class RDL::Logging
 
     [source_line, highlight_line].map do |line|
       source_loc = "#{file_name}:#{range.line}:"
-      unless RDL::Config.instance.disable_log_colors
+      unless no_color
         source_loc = source_loc.colorize(:light_black)
       end
 
