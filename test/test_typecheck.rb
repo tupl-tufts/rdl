a6784ee9ff4da9a56ea38ef4da52e44e2a8de1f7
Geoff Huston <geoffhuston@users.noreply.github.com>
2020-05-26 19:22:07 -0400

    added note_type test (#99)

--- test/test_typecheck.rb
+++ test/test_typecheck.rb
@@ -2138,4 +2138,22 @@ class TestTypecheck < Minitest::Test
       end
     }
   end
+
+  # From https://stackoverflow.com/a/22777806
+  def capture_stdout
+    stdout = $stdout
+    $stdout = StringIO.new
+    yield
+    $stdout.string
+  ensure
+    $stdout = stdout
+  end
+
+  def test_note_type
+    output = capture_stdout do
+      do_tc "RDL.note_type Hash.new"
+    end
+    output = output.lines.map(&:chomp)
+    assert_equal "(string):1:15: note: Type is `Hash'", output[0]
+  end
 end
