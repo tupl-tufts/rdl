e8ca80f11234903bc83e523efd008558d9793380
Mark Aldrich <mark@mwaldrich.io>
2022-06-29 14:35:45 -0400

    test

--- lib/types/core/securerandom.rb
+++ lib/types/core/securerandom.rb
@@ -1,3 +1,4 @@
 RDL.nowrap :SecureRandom
 
-RDL.type :SecureRandom, :hex, '(?{ n: *Number }) -> String'
+RDL.type SecureRandom, :hex, '(?{ n: Number }) -> String'
+RDL.type SecureRandom, :hex, '(?Number) -> String'
