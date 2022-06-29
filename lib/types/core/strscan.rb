0793088ce47696a7a1f902a184f0eaf471163a96
Mark Aldrich <mark@mwaldrich.io>
2022-06-29 13:50:02 -0400

    test

--- lib/types/core/strscan.rb
+++ lib/types/core/strscan.rb
@@ -4,3 +4,8 @@ RDL.type :StringScanner, 'self.new', '(String, ?%bool) -> StringScanner'
 RDL.type :StringScanner, :eos?, '() -> %bool'
 RDL.type :StringScanner, :scan, '(Regexp) -> String'
 RDL.type :StringScanner, :getch, '() -> String'
+
+
+puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+puts 'Strscan types loaded!'
+puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
