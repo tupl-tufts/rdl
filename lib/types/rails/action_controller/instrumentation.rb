5529d401f08dc300524d54a0fc0c5e49a7d55860
Mark Aldrich <mark@mwaldrich.io>
2022-06-30 12:07:44 -0400

    test

--- lib/types/rails/action_controller/instrumentation.rb
+++ lib/types/rails/action_controller/instrumentation.rb
@@ -1,6 +1,7 @@
 RDL.nowrap :'ActionController::Instrumentation'
 RDL.type :'ActionController::Instrumentation', :render, '(String or Symbol) -> Array<String>'
 RDL.type :'ActionController::Instrumentation', :render, '(?String or Symbol, {content_type: ?String, json: ?(String or ApplicationRecord), layout: ?%bool or String, action: ?String or Symbol, location: ?String, nothing: ?%bool, file: ?String, text: ?[to_s: () -> String], status: ?Symbol, content_type: ?String, formats: ?Symbol or Array<Symbol>, locals: ?Hash<Symbol, x>}) -> Array<String>'
+RDL.type :'ActionController::Instrumentation', :render, '(?String or Symbol, {content_type: ?String, json: ?%any, layout: ?%bool or String, action: ?String or Symbol, location: ?String, nothing: ?%bool, file: ?String, text: ?[to_s: () -> String], status: ?Symbol, content_type: ?String, formats: ?Symbol or Array<Symbol>, locals: ?Hash<Symbol, x>}) -> Array<String>'
 RDL.type :'ActionController::Instrumentation', :redirect_to, '(?(String or Symbol or ActiveRecord::Base), {controller: ?String, action: ?String, notice: ?String, alert: ?String}) -> String'
 
 #RDL.type :'ActionController::Instrumentation', :redirect_to, '({controller: ?String, action: String, notice: ?String, alert: ?String}) -> String' ## When no first argument is provided, `action` must be present in options.
