d5af04ddcb7aed42bfb32062f95f642bcdee4b5f
Mark Aldrich <mark@mwaldrich.io>
2022-06-30 12:16:45 -0400

    test

--- lib/types/rails/action_controller/instrumentation.rb
+++ lib/types/rails/action_controller/instrumentation.rb
@@ -1,6 +1,8 @@
 RDL.nowrap :'ActionController::Instrumentation'
 RDL.type :'ActionController::Instrumentation', :render, '(String or Symbol) -> Array<String>'
-RDL.type :'ActionController::Instrumentation', :render, '(?String or Symbol, {content_type: ?String, json: ?(String or ApplicationRecord), layout: ?%bool or String, action: ?String or Symbol, location: ?String, nothing: ?%bool, file: ?String, text: ?[to_s: () -> String], status: ?Symbol, content_type: ?String, formats: ?Symbol or Array<Symbol>, locals: ?Hash<Symbol, x>}) -> Array<String>'
+RDL.type :'ActionController::Instrumentation', :render, '(?String or Symbol, {content_type: ?String, json: ?String, layout: ?%bool or String, action: ?String or Symbol, location: ?String, nothing: ?%bool, file: ?String, text: ?[to_s: () -> String], status: ?Symbol, content_type: ?String, formats: ?Symbol or Array<Symbol>, locals: ?Hash<Symbol, x>}) -> Array<String>'
+
+# %any version of `render`
 RDL.type :'ActionController::Instrumentation', :render, '(?String or Symbol, {content_type: ?String, json: ?%any, layout: ?%bool or String, action: ?String or Symbol, location: ?String, nothing: ?%bool, file: ?String, text: ?[to_s: () -> String], status: ?Symbol, content_type: ?String, formats: ?Symbol or Array<Symbol>, locals: ?Hash<Symbol, x>, include: ?%any}) -> Array<String>'
 RDL.type :'ActionController::Instrumentation', :redirect_to, '(?(String or Symbol or ActiveRecord::Base), {controller: ?String, action: ?String, notice: ?String, alert: ?String}) -> String'
 
