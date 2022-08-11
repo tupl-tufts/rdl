98bba55f404b69bfd4bdd3a1c45368dbbac3ea63
Mark Aldrich <mark@mwaldrich.io>
2022-08-11 13:30:30 -0400

    tweaked some types

--- lib/types/rails/action_controller/instrumentation.rb
+++ lib/types/rails/action_controller/instrumentation.rb
@@ -4,7 +4,7 @@ RDL.nowrap :'ActionController::Instrumentation'
 
 # %any version of `render`
 #before: 
-RDL.type :'ActionController::Instrumentation', :render, '(?String or Symbol, {content_type: ?String, json: ?%any, layout: ?%bool or String, action: ?String or Symbol, location: ?String, nothing: ?%bool, file: ?String, text: ?[to_s: () -> String], status: ?Symbol, content_type: ?String, formats: ?Symbol or Array<Symbol>, locals: ?Hash<Symbol, x>, include: ?%any}) -> Array<String>'
+RDL.type :'ActionController::Instrumentation', :render, '(?String or Symbol, {content_type: ?String, json: ?%any, xml: ?%any, layout: ?%bool or String, action: ?String or Symbol, location: ?String, nothing: ?%bool, file: ?String, text: ?[to_s: () -> String], status: ?(Symbol or Numeric), content_type: ?String, formats: ?Symbol or Array<Symbol>, locals: ?Hash<Symbol, x>, include: ?%any}) -> Array<String>'
 #with JSON comp type:
 #RDL.type :'ActionController::Instrumentation', :render, '(?String or Symbol, {content_type: ?String, json: ?%any, layout: ?%bool or String, action: ?String or Symbol, location: ?String, nothing: ?%bool, file: ?String, text: ?[to_s: () -> String], status: ?Symbol, content_type: ?String, formats: ?Symbol or Array<Symbol>, locals: ?Hash<Symbol, x>, include: ?%any}) -> ``DBType.rec_as_json(targs[0].elts[:json])``'
 RDL.type :'ActionController::Instrumentation', :redirect_to, '(?(String or Symbol or ActiveRecord::Base), {controller: ?String, action: ?String, notice: ?String, alert: ?String}) -> String'
