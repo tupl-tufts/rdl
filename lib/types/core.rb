57fadd0640e6001a41c98531935f8eb2316076b6
Mark Aldrich <mark@mwaldrich.io>
2024-05-16 13:15:27 -0400

    Fix `type parameters not defined for Array` bug
    this was triggering in about 1/10 runs. very annoying. the issue stems from the fact that the core types are `require`'d in a random order.

--- lib/types/core.rb
+++ lib/types/core.rb
@@ -1,2 +1,3 @@
 require_relative "core/_aliases.rb" # load type aliases first
+require_relative "core/array.rb" # and Array so we can use Array<T>
 Dir[File.dirname(__FILE__) + "/core/**/*.rb"].each { |f| require f }
