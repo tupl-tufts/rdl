53097c771b7168c3e7bb6940c70cabb6bad3f5ef
Mark Aldrich <mark@mwaldrich.io>
2025-01-29 12:51:05 -0500

    change to I18n.t type sig

--- lib/types/rails/abstract_controller/translation.rb
+++ lib/types/rails/abstract_controller/translation.rb
@@ -1,3 +1,4 @@
 RDL.nowrap :'AbstractController::Translation'
-RDL.type :'AbstractController::Translation', :t, '(Symbol or String, ?Hash) -> String'
+RDL.type :'AbstractController::Translation', :t, '(Symbol or String) -> String'
+RDL.type :'AbstractController::Translation', :t, '(Symbol or String, **%any) -> String' # to allow for arbitrary substitutions
 RDL.type :'AbstractController::Translation', :t, '({one: (Symbol or String), other: (Symbol or String)}, {count: Number}) -> String'
